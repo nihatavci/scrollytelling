bc575ebb907e2cd95e1f1c44ad504644ae450276
nihatavci <afsin@inflownetwork.com>
2026-06-08 08:11:31 +0200

    chore(flow): version bump (margin + per-scene text)

--- admin/ui/app.js
+++ admin/ui/app.js
@@ -4644,7 +4644,7 @@ ${themeLink}
 <body>
 <main id="page-root"></main>
 <script type="module">
-import { render } from '${origin}/js/render.js?v=20260528y';
+import { render } from '${origin}/js/render.js?v=20260528z';
 render();
 <\/script>
 ${veScript}</body></html>`;
